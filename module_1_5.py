e2a87c39968b2b042eceea110e967d27fed0209e
Артём Салимов <arsa-17@bk.ru>
2024-09-12 22:27:38 +0500

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,8 @@
+immutable_var = 1, 2, 'Green', 'Forest'
+print(' - Выводим кортеж immutable_var на экран -', immutable_var)
+#'Из лекции 'Изменяемые и неизменяемые объекты Кортежи' мы
+#выяснили, что данные в кортежах изменять нельзя')
+mutable_list = [1, 2, 'apple', 'banana']
+mutable_list [0] = True
+mutable_list [2] = 'cherry'
+print(' - Выводим на экран измененный список Mutable_list -', mutable_list)
